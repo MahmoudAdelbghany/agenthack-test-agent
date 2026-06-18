8ae95db21772f39ac8c7a73d4690248c7bb290be
Mahmoud Abdelghany <mahmoud_a_rageh@yahoo.com>
2026-06-18 13:30:33 +0300

    Auto-heal: Fix test failure

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 def add(a, b):
-    # Bug: returns multiplication instead of addition
-    return a * b
+    # Returns the sum of a and b
+    return a + b
 
 def test_add():
     assert add(2, 3) == 5
